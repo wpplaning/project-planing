6334e4595a6bce0ad130787b6df5294fcaa8edc2
Disira Thihan <87310369+it21070358@users.noreply.github.com>
2023-11-02 10:50:51 +0530

    last update

--- src/config/firestore.js
+++ src/config/firestore.js
@@ -6,13 +6,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAyxgFzQrnhdKmQvtL4Tc6-Ls8L7wiTiqg",
-  authDomain: "ministry-test-ecfc8.firebaseapp.com",
-  projectId: "ministry-test-ecfc8",
-  storageBucket: "ministry-test-ecfc8.appspot.com",
-  messagingSenderId: "124707917389",
-  appId: "1:124707917389:web:795e5f444c4e3ebf825604",
-  measurementId: "G-RDLFV26QDN",
+  apiKey: "AIzaSyDtP4mTd1u_HUXQVWsSV1BdnVjLGhWXHpQ",
+  authDomain: "project-palning.firebaseapp.com",
+  projectId: "project-palning",
+  storageBucket: "project-palning.appspot.com",
+  messagingSenderId: "58209331296",
+  appId: "1:58209331296:web:417c30f73d4bc6f7fb7b05",
+  measurementId: "G-YZMTPQH0H5"
 };
 
 // Initialize Firebase
